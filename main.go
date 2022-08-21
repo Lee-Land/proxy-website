21c49d2248bf360aa79e0f6b308aba6cbe22f8c9
xiamingjie <xiamingjie@wepie.com>
2022-08-21 18:39:24 +0800

    initial commit

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"time"
+)
+
+func indexHtml(c *gin.Context) {
+	c.HTML(http.StatusOK, "index.html", gin.H{
+		"title": "Welcome to Liu-Proxy Website",
+	})
+}
+
+func main() {
+	r := gin.Default()
+	r.GET("/checkup", func(c *gin.Context) {
+		c.String(http.StatusOK, time.Now().String())
+	})
+
+	r.LoadHTMLGlob("static/template/*")
+	r.GET("/", indexHtml)
+	r.GET("/index", indexHtml)
+
+	_ = r.Run(":8080")
+}
